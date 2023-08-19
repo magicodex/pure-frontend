215152bc9e4be2edafedb2c8820d6c6bc926013c
magicodex <zhuangmaohui@163.com>
2023-08-20 04:14:21 +0800

    添加视图生命周期

--- src/view/view-manager.js
+++ src/view/view-manager.js
@@ -20,16 +20,18 @@ ViewManager.viewScopes = {};
 /**
  * @description 返回指定的视图作用域
  * @param {string} viewName 视图名
+ * @param {boolean} [allowCreate=true]
  * @returns {object}
  */
-ViewManager.getViewScope = function (viewName) {
+ViewManager.getViewScope = function (viewName, allowCreate) {
   if (!Utils.isString(viewName)) {
     throw new Error('argument#0 "viewName" required string');
   }
 
+  allowCreate = !(allowCreate === false);
   var viewScope = ViewManager.viewScopes[viewName];
 
-  if (Utils.isNullOrUndefined(viewScope)) {
+  if (Utils.isNullOrUndefined(viewScope) && allowCreate) {
     viewScope = {};
     ViewManager.viewScopes[viewName] = viewScope;
   }
@@ -75,12 +77,20 @@ ViewManager.loadView = function (url) {
   var selector = Utils.formatString('[{0}="{1}"]',
     [Global.config.viewStatusAttributeName, 'show']);
   var jqElement = jQuery(selector);
+
+  if (jqElement.length <= 0) {
+    return;
+  }
+
+  ViewManager.onViewCreate(jqElement);
   jqElement.attr(Global.config.viewStatusAttributeName, 'loading');
+  jqElement.removeAttr(Global.config.viewIndexAttributeName);
 
   var viewLoader = new ViewLoader(jqElement[0]);
   viewLoader.loadView(url);
 
   jqElement.attr(Global.config.viewStatusAttributeName, 'show');
+  ViewManager.onViewCreate(jqElement);
 };
 
 /**
@@ -107,6 +117,45 @@ ViewManager.popView = function (url) {
   ViewManager.loadView(url);
 };
 
+ViewManager.onViewCreate = function (jqView) {
+  var viewIndex = jqView.attr(Global.config.viewIndexAttributeName);
+
+  if (Utils.isNotEmptyString(viewIndex)) {
+    var viewScope = ViewManager.getViewScope(viewIndex, false);
+
+    if (!Utils.isNullOrUndefined(viewScope)) {
+      var onViewCreate = viewScope.onViewCreate;
+      if (!Utils.isNullOrUndefined(onViewCreate)) {
+        onViewCreate();
+      }
+    }
+  }
+};
+
+ViewManager.onViewPause = function (jqView) {
+
+};
+
+ViewManager.onViewResume = function (jqView) {
+
+};
+
+ViewManager.onViewDestroy = function (jqView) {
+  var viewIndex = jqView.attr(Global.config.viewIndexAttributeName);
+
+  if (Utils.isNotEmptyString(viewIndex)) {
+    var viewScope = ViewManager.getViewScope(viewIndex, false);
+
+    if (!Utils.isNullOrUndefined(viewScope)) {
+      var onViewDestroy = viewScope.onViewDestroy;
+      if (!Utils.isNullOrUndefined(onViewDestroy)) {
+        onViewDestroy();
+      }
+
+      ViewManager.removeViewScope(viewIndex);
+    }
+  }
+};
 
 /* SOURCE-CODE-END */
 
