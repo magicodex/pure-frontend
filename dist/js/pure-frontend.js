ab9e325e883186c5ccfcf43608df35aead866a90
magicodex <zhuangmaohui@163.com>
2023-09-30 04:49:16 +0800

    发布v1.2.4

--- dist/js/pure-frontend.js
+++ dist/js/pure-frontend.js
@@ -1,7 +1,7 @@
 "use strict";
 
 /*!
- * pure-frontend v1.2.4-dev (https://gitee.com/magicodex/pure-frontend)
+ * pure-frontend v1.2.4 (https://gitee.com/magicodex/pure-frontend)
  * Licensed under MIT (https://gitee.com/magicodex/pure-frontend/blob/master/LICENSE)
  */
 
